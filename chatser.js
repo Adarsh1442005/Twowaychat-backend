9c9537776b1415ab2feccb18206c30e3ece32629
Adarsh1442005 <btech60077.23@bitmesra.ac.in>
2025-06-05 02:07:55 +0530

    Update chatser.js

--- chatser.js
+++ chatser.js
@@ -112,7 +112,7 @@ const server=http.createServer(app);
 
 const Io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://twowaychat-backend.onrender.com",
         methods: ["GET", "POST"],
     },
 });
